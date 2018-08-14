314906cd99b85df0a108110c9164edc167c0893c
qzhu <Goblin22>
2018-08-14 16:17:48 -0400

    React sign up and login page, switchable

--- public/react/index.js
+++ public/react/index.js
@@ -1,27 +1,35 @@
-class Login extends React.Component {
+class Signup extends React.Component {
     render() {
-        return (
-            <div>
-                <h4>Login</h4>
-                <div>Username:</div><div><input type="text"/></div>
-                <div>Password:</div><div><input type="password"/></div>
-                <div><button>Login</button><button>Go to sign up</button></div>
-            </div>
-        );
+        // console.log("sign up:"+this.props.login);
+        if(this.props.login)
+            return (<div></div>);
+        else
+            return (
+                <div>
+                    <h4>Signup</h4>
+                    <div>Username:</div><div><input type="text"/></div>
+                    <div>Password:</div><div><input type="password"/></div>
+                    <div>Email:</div><div><input type="email"/></div>
+                    <div><button>Sign up</button><button onClick={this.props.toggleLogin}>Go to login</button></div>
+                </div>
+            );
     }
 }
 
-class Signup extends React.Component {
+class Login extends React.Component {
     render() {
-        return (
-            <div>
-                <h4>Signup</h4>
-                <div>Username:</div><div><input type="text"/></div>
-                <div>Password:</div><div><input type="password"/></div>
-                <div>Email:</div><div><input type="email"/></div>
-                <div><button>Sign up</button><button>Go to login</button></div>
-            </div>
-        );
+        // console.log("login:"+this.props.login);
+        if(this.props.login)
+            return (
+                <div>
+                    <h4>Login</h4>
+                    <div>Username:</div><div><input type="text"/></div>
+                    <div>Password:</div><div><input type="password"/></div>
+                    <div><button>Login</button><button onClick={this.props.toggleLogin}>Go to sign up</button></div>
+                </div>
+            );
+        else
+            return (<div></div>);
     }
 }
 
@@ -32,19 +40,35 @@ class Footer extends React.Component {
 }
 
 class Greeting extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            login: true
+        };
+    }
+
+    toggleLoginSignup(){
+        // console.log("before click:"+this.state.login);
+
+        //have to call setState to force rendering sub doms
+        this.state.login ? this.setState({'login':false}) : this.setState({'login':true});
+        // console.log("after click:"+this.state.login);
+    }
 
     renderLogin(){
-        return <Login />;
+        return <Login login = {this.state.login} toggleLogin = {() => this.toggleLoginSignup()}/>;
     }
 
     render() {
+        const login = this.state.login;
+
         return (
             //everything must be enclosed within one element
             <div>
                 <h3>Welcome to XiaBB</h3>
                 {this.renderLogin()}
                 {/* must use {} expression to render sub dom */}
-                {<Signup />}
+                {<Signup login = {login} toggleLogin = {this.toggleLoginSignup.bind(this)}/>}
                 {<Footer />}
             </div>
 
